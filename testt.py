2b71d8efe9fafdf1064f26d0782fbd0523b96fe6
Aleksey <moon41@yandex.ru>
2025-02-13 16:39:41 +0300

    four

--- testt.py
+++ testt.py
@@ -16,17 +16,25 @@ from playwright.sync_api import Page, expect, sync_playwright
 #     # Expects page to have a heading with the name of Installation.
 #     expect(page.get_by_role("heading", name="Installation")).to_be_visible()
 
-def open_brow():
-    with sync_playwright() as op:
-        browser = op.chromium.launch(headless=False)
+def login_page():
+    with sync_playwright() as sp:
+        browser = sp.chromium.launch(headless=False)
         page = browser.new_page()
-        page.goto('https://playwright.dev/')
-        expect(page).to_have_title(re.compile('Playwright'))
-        page.get_by_role('link', name='Get started').click()
-        expect(page.get_by_role('heading', name='Installation'))
+        page.goto('https://test02-tkp-lko.secgw.ru/#/login')
+        expect(page).to_have_title(re.compile('ЛК ТКП'))
+        expect(page.get_by_role('heading', name='Вход')).to_be_visible()
+
+        # page.fill('input[placeholder="Логин"]', 'Admin')
+        page.get_by_placeholder('Логин').fill('Admin')
+        # page.fill('input[placeholder="Пароль"]', '123')
+        page.get_by_placeholder('Пароль').fill('123')
+        
+        # page.get_by_role('button', name='Войти').click()
+        page.click('text=Войти')
+        expect(page.get_by_role('heading', name='Главная страница')).to_be_visible()
         page.screenshot(path='./demo.png')
-        browser.close()
+        # browser.close()
 
 
-open_brow()
+login_page()
 
